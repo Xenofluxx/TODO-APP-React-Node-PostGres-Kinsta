65e7b5ffb5d018a5322047bf55845c74e790e569
Susana Hueso Devís <susanainu@hotmail.com>
2023-10-31 17:35:29 +0100

    connected back and front

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,19 @@ import { Styled } from "./App.styles";
 import { useEffect } from "react";
 
 export const App = () => {
+  const userEmail = "susana.huesodevis@gmail.com";
+  useEffect(() => {
+    getData();
+  }, []);
+  const getData = async () => {
+    try {
+      const response = await fetch(`http://localhost:8000/todos/${userEmail}`);
+      const json = await response.json();
+      console.log(json);
+    } catch (err) {
+      console.error(err);
+    }
+  };
   useEffect(() => {
     console.log(new Date());
   });
--- server/server.js
+++ server/server.js
@@ -2,16 +2,24 @@
 const PORT = process.env.PORT ?? 8000;
 const express = require("express");
 const app = express();
+const cors = require('cors');
 const pool = require("./db");
-/*app.get("/", (req, res) => {
+
+app.use(cors());
+app.get("/", (req, res) => {
   res.send("GET Request Called");
-});*/
-app.get("/todos", async (req, res) => {
+});
+app.get("/todos/:userEmail", async (req, res) => {
+  const {userEmail} = req.params;
+  console.log(userEmail);
   try {
-    const todos = await pool.query("SELECT * FROM todos");
+    const todos = await pool.query(
+      "SELECT * FROM todos WHERE user_email = $1",
+      [userEmail]
+    );
     res.json(todos.rows);
   } catch (err) {
-    //console.error(err);
+    console.error(err);
   }
 });
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
